c19f0bf6fcd3ae2e40f6a795cd263d6a607c2158
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2026-02-01 14:40:11 +1100

    fix: `Post` not actually thread-safe.

--- src/ftxui/component/task_queue.hpp
+++ src/ftxui/component/task_queue.hpp
@@ -31,6 +31,7 @@ struct TaskQueue {
   mutable std::mutex mutex_;
   std::queue<PendingTask> immediate_tasks_;
   std::priority_queue<PendingTask> delayed_tasks_;
+  std::mutex mutex_;
 };
 
 }  // namespace ftxui::task
